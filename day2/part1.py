ca2335921dd4ab7ee348752648fe36573e96b68b
Cyrus Sadeghi <cyrus.cse@gmail.com>
2020-12-02 11:50:52 -0500

    Day 2 solutions

---
+++ day2/part1.py
@@ -0,0 +1,17 @@
+def main():
+    passwords = open('input.txt', 'r')
+    valid_passwords = 0
+
+    for password in passwords:
+        pw_entry = password.split()
+        pw_restrict = pw_entry[1][0]
+        pw_data = pw_entry[2]
+        pw_range_min = int(pw_entry[0].split('-')[0])
+        pw_range_max = int(pw_entry[0].split('-')[1])
+        
+        if pw_data.count(pw_restrict) >= pw_range_min and pw_data.count(pw_restrict) <= pw_range_max:
+            valid_passwords = valid_passwords + 1
+
+    print(valid_passwords)
+
+main()
---
+++ day2/part2.py
@@ -0,0 +1,19 @@
+def main():
+    passwords = open('input.txt', 'r')
+    valid_passwords = 0
+
+    for password in passwords:
+        pw_entry = password.split()
+        pw_restrict = pw_entry[1][0]
+        pw_data = pw_entry[2]
+        pw_pos_1 = int(pw_entry[0].split('-')[0])
+        pw_pos_2 = int(pw_entry[0].split('-')[1])
+
+        pos_1_valid = pw_data[pw_pos_1 - 1] == pw_restrict
+        pos_2_valid = pw_data[pw_pos_2 - 1] == pw_restrict
+        if (pos_1_valid == True and pos_2_valid == False) or (pos_1_valid == False and pos_2_valid == True):
+            valid_passwords = valid_passwords + 1
+
+    print(valid_passwords)
+
+main()
